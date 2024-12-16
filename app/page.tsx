3479ae6f985e667e3e9b20ca90f9a4b229fcb7c1
Alex <lamp0020@hz.nl>
2024-12-16 08:49:17 +0100

    Added Loading Spinner

--- app/page.tsx
+++ app/page.tsx
@@ -6,12 +6,14 @@ import Image from 'next/image';
 import Link from 'next/link';
 import Filter from '@/components/Filter';
 import { AiOutlineHeart, AiFillHeart } from 'react-icons/ai';
+import { ClipLoader } from 'react-spinners';
 
 export default function Home() {
   const [searchResults, setSearchResults] = useState<Podcast[]>([]);
   const [filteredResults, setFilteredResults] = useState<Podcast[]>([]);
   const [showAll, setShowAll] = useState(false);
   const [favorites, setFavorites] = useState<Podcast[]>([]);
+  const [loading, setLoading] = useState(true);
 
   const filters = {
     frequentie: ['Dagelijks', 'Wekelijks'],
@@ -43,6 +45,7 @@ export default function Home() {
         }
         const data = await response.json();
         setSearchResults(data.items);
+        setLoading(false); // Set loading to false after data is fetched
       } catch (error) {
         console.error(error);
       }
@@ -94,80 +97,86 @@ export default function Home() {
         selectedFilters={selectedFilters}
         setSelectedFilters={setSelectedFilters}
       />
-        
-      {favorites.length > 0 ? (
-        <div className='my-12'>
-          <h2 className="text-xl font-semibold mt-6">Favoriete Podcasts</h2>
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 mt-4">
-            {favorites.map((show) => (
-              <div key={show.id} className="border rounded-lg overflow-hidden">
-                <Image
-                  src={show.images[0]?.url || '/placeholder-image.jpg'}
-                  alt={show.name}
-                  width={500}
-                  height={500}
-                  className="w-full h-48 object-cover"
-                />
-                <div className="p-4">
-                  <h2 className="text-xl font-semibold">{show.name}</h2>
-                  <button
-                    onClick={() => toggleFavorite(show)}
-                    className="mt-2 text-sm text-red-500"
-                  >
-                    Verwijder van Favorieten
-                  </button>
-                </div>
-              </div>
-            ))}
-          </div>
+
+      {loading ? (
+        <div className="flex justify-center items-center h-64">
+          <ClipLoader size={50} color="#22C55D" />
         </div>
       ) : (
-        <></>
-      )}
-
-      <h2 className="text-xl font-semibold mt-6">Alle Podcasts</h2>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 mt-4">
-        {visiblePodcasts.map((show: Podcast) => (
-          <Link 
-            href={show.external_urls.spotify} 
-            target="_blank" 
-            rel="noopener noreferrer"
-            key={show.id}
-          >
-            <div className='border rounded-lg flex-col h-full hover:shadow-[#ffffff4d] hover:shadow-md hover:transform overflow-hidden block hover:scale-102 ease-in transition duration-200'>
-              <Image 
-                src={show.images[0]?.url || '/placeholder-image.jpg'} 
-                alt={show.name} 
-                width={500} 
-                height={500} 
-                className="w-full h-48 object-cover rounded-t-lg" 
-              />
-              <div className="p-4">
-                <h2 className="text-xl font-semibold">{show.name}</h2>
-                <p className="text-sm mt-2 opacity-70">
-                  {show.description.length > 150 
-                    ? `${show.description.substring(0, 150)}...` 
-                    : show.description}
-                </p>
-                <button onClick={() => toggleFavorite(show)} className='mt-6'>
-                    {isFavorite(show) ? (
-                    <AiFillHeart className="text-red-500 text-2xl cursor-pointer transition duration-300 ease-in-out transform hover:scale-110" />
-                    ) : (
-                    <AiOutlineHeart className="text-gray-500 text-2xl cursor-pointer transition duration-300 ease-in-out transform hover:text-green-500 hover:scale-110" />
-                    )}
-                </button>
+        <>
+          {favorites.length > 0 && (
+            <div className='my-12'>
+              <h2 className="text-xl font-semibold mt-6">Favoriete Podcasts</h2>
+              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 mt-4">
+                {favorites.map((show) => (
+                  <div key={show.id} className="border rounded-lg overflow-hidden">
+                    <Image
+                      src={show.images[0]?.url || '/placeholder-image.jpg'}
+                      alt={show.name}
+                      width={500}
+                      height={500}
+                      className="w-full h-48 object-cover"
+                    />
+                    <div className="p-4">
+                      <h2 className="text-xl font-semibold">{show.name}</h2>
+                      <button
+                        onClick={() => toggleFavorite(show)}
+                        className="mt-2 text-sm text-red-500"
+                      >
+                        Verwijder van Favorieten
+                      </button>
+                    </div>
+                  </div>
+                ))}
               </div>
             </div>
-          </Link>
-        ))}
-      </div>
-      {!showAll && filteredResults.length > 8 && (
-        <button
-          onClick={() => setShowAll(true)}
-          className="mt-4 bg-white border text-black p-4 rounded-lg hover:opacity-95 font-semibold transition-90"
-        >
-          Bekijk meer
-        </button>
+          )}
+
+          <h2 className="text-xl font-semibold mt-6">Alle Podcasts</h2>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 mt-4">
+            {visiblePodcasts.map((show: Podcast) => (
+              <Link 
+                href={show.external_urls.spotify} 
+                target="_blank" 
+                rel="noopener noreferrer"
+                key={show.id}
+              >
+                <div className='border rounded-lg flex-col h-full hover:shadow-[#ffffff4d] hover:shadow-md hover:transform overflow-hidden block hover:scale-102 ease-in transition duration-200'>
+                  <Image 
+                    src={show.images[0]?.url || '/placeholder-image.jpg'} 
+                    alt={show.name} 
+                    width={500} 
+                    height={500} 
+                    className="w-full h-48 object-cover rounded-t-lg" 
+                  />
+                  <div className="p-4">
+                    <h2 className="text-xl font-semibold">{show.name}</h2>
+                    <p className="text-sm mt-2 opacity-70">
+                      {show.description.length > 150 
+                        ? `${show.description.substring(0, 150)}...` 
+                        : show.description}
+                    </p>
+                    <button onClick={() => toggleFavorite(show)} className='mt-6'>
+                        {isFavorite(show) ? (
+                        <AiFillHeart className="text-red-500 text-2xl cursor-pointer transition duration-300 ease-in-out transform hover:scale-110" />
+                        ) : (
+                        <AiOutlineHeart className="text-gray-500 text-2xl cursor-pointer transition duration-300 ease-in-out transform hover:text-green-500 hover:scale-110" />
+                        )}
+                    </button>
+                  </div>
+                </div>
+              </Link>
+            ))}
+          </div>
+          {!showAll && filteredResults.length > 8 && (
+            <button
+              onClick={() => setShowAll(true)}
+              className="mt-4 bg-white border text-black p-4 rounded-lg hover:opacity-95 font-semibold transition-90"
+            >
+              Bekijk meer
+            </button>
+          )}
+        </>
       )}
     </div>
   );
