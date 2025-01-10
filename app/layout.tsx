b12d930323fa0cefc9c143591ba23fc497a13ed0
Alex <lamp0020@hz.nl>
2025-01-10 08:49:30 +0100

    Updated Default Theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -53,7 +53,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body>
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
+          defaultTheme="light"
           enableSystem
           disableTransitionOnChange
           forcedTheme={undefined}
