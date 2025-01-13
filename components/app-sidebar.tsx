a7b02558bbba595f126f8e567296734dc2a62a2c
Alex <lamp0020@hz.nl>
2025-01-13 11:42:38 +0100

    Fixed Automatically Closing Sidebar on Mobile

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -1,8 +1,8 @@
-"use client"
+"use client";
 
-import * as React from "react"
-import Link from "next/link"
-import { HelpCircleIcon, Home, Library, Phone } from 'lucide-react'
+import * as React from "react";
+import Link from "next/link";
+import { HelpCircleIcon, Home, Library, Phone } from "lucide-react";
 import {
   Sidebar,
   SidebarContent,
@@ -11,16 +11,18 @@ import {
   SidebarMenu,
   SidebarMenuItem,
   SidebarMenuButton,
-} from "@/components/ui/sidebar"
-// import DailyVerse from "@/components/DailyVerse"
+  useSidebar,
+} from "@/components/ui/sidebar";
 
 export function AppSidebar() {
+  const { setOpenMobile } = useSidebar();
+
   return (
     <Sidebar className="">
       <div className="h-full bg-gray-100 dark:bg-black">
         <SidebarHeader>
           <div className="px-6 pt-6">
-            <Link href={"/"}>
+            <Link href="/">
               <h1 className="text-3xl font-bold text-green-500">Christelijke</h1>
             </Link>
             <p className="text-sm text-gray-400 dark:text-gray-500">Podcasts</p>
@@ -30,7 +32,7 @@ export function AppSidebar() {
           <SidebarMenu>
             <SidebarMenuItem>
               <SidebarMenuButton asChild>
-                {/* <DailyVerse /> */}
+                {/* Add content here if needed */}
               </SidebarMenuButton>
             </SidebarMenuItem>
           </SidebarMenu>
@@ -38,32 +40,48 @@ export function AppSidebar() {
         <SidebarFooter className="p-6 text-white dark:text-gray-300 hover:text-gray-300">
           <SidebarMenu>
             <SidebarMenuItem className="py-2">
-              <SidebarMenuButton className="flex items-center dark:text-white text-black hover:text-gray-600 dark:hover:text-gray-400">
-                <Link href="/" className="flex items-center">
+              <SidebarMenuButton className="flex items-center">
+                <Link
+                  href="/"
+                  onClick={() => setOpenMobile(false)}
+                  className="flex items-center"
+                >
                   <Home className="mr-4 text-gray-800 dark:text-gray-300" />
                   Home
                 </Link>
               </SidebarMenuButton>
             </SidebarMenuItem>
             <SidebarMenuItem className="py-2">
-              <SidebarMenuButton className="flex items-center dark:text-white text-black hover:text-gray-600 dark:hover:text-gray-400">
-                <Link href="/favorieten" className="flex items-center">
+              <SidebarMenuButton className="flex items-center">
+                <Link
+                  href="/favorieten"
+                  onClick={() => setOpenMobile(false)}
+                  className="flex items-center"
+                >
                   <Library className="mr-4 text-gray-800 dark:text-gray-300" />
                   Favorieten
                 </Link>
               </SidebarMenuButton>
             </SidebarMenuItem>
             <SidebarMenuItem className="py-2">
-              <SidebarMenuButton className="flex items-center dark:text-white text-black hover:text-gray-600 dark:hover:text-gray-400">
-                <Link href="/contact" className="flex items-center">
+              <SidebarMenuButton className="flex items-center">
+                <Link
+                  href="/contact"
+                  onClick={() => setOpenMobile(false)}
+                  className="flex items-center"
+                >
                   <Phone className="mr-4 text-gray-800 dark:text-gray-300" />
                   Contact
                 </Link>
               </SidebarMenuButton>
             </SidebarMenuItem>
             <SidebarMenuItem className="py-2">
-              <SidebarMenuButton className="flex items-center dark:text-white text-black hover:text-gray-600 dark:hover:text-gray-400">
-                <Link href="/help" className="flex items-center">
+              <SidebarMenuButton className="flex items-center">
+                <Link
+                  href="/help"
+                  onClick={() => setOpenMobile(false)}
+                  className="flex items-center"
+                >
                   <HelpCircleIcon className="mr-4 text-gray-800 dark:text-gray-300" />
                   Help
                 </Link>
@@ -73,5 +91,5 @@ export function AppSidebar() {
         </SidebarFooter>
       </div>
     </Sidebar>
-  )
+  );
 }
